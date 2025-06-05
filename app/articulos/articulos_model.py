6efbbe07b5cc638ed5abf675bbc7716e7afc9c73
leotorres <leo_escobar11@hotmail.com>
2025-06-05 20:57:05 -0300

    2 commit

--- app/articulos/articulos_model.py
+++ app/articulos/articulos_model.py
@@ -1,18 +1,20 @@
 from ..database.conect_db import ConectDB
 from ..marcas.marcas_model import MarcasModel as Marca
 from ..proveedores.proveedores_model import ProveedoresModel as Proveedor
+from   ..categorias.categorias_model import CategoriasModel as Categorias
 import pymysql
 
 class ArticulosModel():
 
     def __init__(self, id:int=0, descripcion:str="",marca:Marca = None ,
-                 precio:float=0.0,proveedor:Proveedor=None ,stock:int=0 ):
+                 precio:float=0.0,proveedor:Proveedor=None ,stock:int=0,categorias:list[Categorias]=None):
         self.id = id
         self.descripcion = descripcion
         self.precio = precio
         self.stock = stock
         self.marca = marca
         self.proveedor = proveedor
+        self.categorias = categorias
         
     def serializar(self) -> dict:
         return {
@@ -21,66 +23,97 @@ class ArticulosModel():
                 'precio': self.precio,
                 'stock': self.stock,
                 'marca': self.marca.serializar() if self.marca else None,
-                'proveedor': self.proveedor.serializar() if self.proveedor else None           
-        }
+                'proveedor': self.proveedor.serializar() if self.proveedor else None, 
+                'categorias': [categoria.serializar() for categoria in self.categorias]
+         
+        }  
     
     @staticmethod
     def deserializar(data:dict):
         return ArticulosModel(
-            id=data['id'], 
-            descripcion=data['descripcion'],
-            precio=data['precio'], 
-            stock=data['stock'],
-            marca=data['marca'],
-            proveedor=data['proveedor'] 
+          **data  
         )
         
     @staticmethod
-    def get_all():
+    def get_all() -> list[dict]:
      cnx = ConectDB.connect()
-     with cnx.cursor(pymysql.cursors.DictCursor) as cursor:
-        try:
+     try:
+        with cnx.cursor(pymysql.cursors.DictCursor) as cursor:
+            cursor.execute("SELECT * FROM categorias")
+            todas_categorias_rows = cursor.fetchall()
+            todas_categorias = [Categorias.deserializar(cat) for cat in todas_categorias_rows]
+
+           
             cursor.execute("SELECT * FROM articulos")
             rows = cursor.fetchall()
             articulos = []
             for row in rows:
+              
                 marca = Marca(id=row['marca_id']).get_by_id()
                 proveedor = Proveedor(id=row['proveedor_id']).get_by_id()
+                marca = Marca.deserializar(marca) if marca else None
+                proveedor = Proveedor.deserializar(proveedor) if proveedor else None
+
+                
                 articulo = ArticulosModel(
                     id=row['id'],
                     descripcion=row['descripcion'],
                     precio=row['precio'],
                     stock=row['stock'],
-                    marca=Marca.deserializar(marca) if marca else None,
-                    proveedor=Proveedor.deserializar(proveedor) if proveedor else None
+                    marca=marca,
+                    proveedor=proveedor,
+                    categorias=todas_categorias
                 )
                 articulos.append(articulo.serializar())
             return articulos
-        except Exception as exc:
-            return {'mensaje': f" error al listar articulos {exc}"}
-        finally:
-            cnx.close()
-    @staticmethod       
-    def get_by_id(self):
+     except Exception as exc:
+        print(f"Error al listar articulos: {exc}")
+        return {'mensaje': f" error al listar articulos {exc}"}
+     finally:
+        cnx.close()
+        
+    def get_by_id(self)->dict:
      cnx = ConectDB.connect()
-     with cnx.cursor(pymysql.cursors.DictCursor) as cursor:
-        try:
-            cursor.execute("SELECT * FROM articulos where id = %s", (self.id,))
+     try:
+        with cnx.cursor(pymysql.cursors.DictCursor) as cursor:
+            cursor.execute("SELECT * FROM articulos WHERE id = %s", (self.id,))
             row = cursor.fetchone()
             print("Row encontrado:", row)
-            if row:
-                marca = Marca(id=row['marca_id']).get_by_id()
-                proveedor = Proveedor(id=row['proveedor_id']).get_by_id()
-                row['marca'] = Marca.deserializar(marca) if marca else None
-                row['proveedor'] = Proveedor.deserializar(proveedor) if proveedor else None
-                row = ArticulosModel.deserializar(row)
-                return row.serializar()
-            return False
-        except Exception as exc:
-            print("Error en get_by_id:", exc)
-            return {'mensaje':f" error buscar un articulo {exc}"}
-        finally:
-            cnx.close() 
+            if not row:
+                return False
+
+            
+            marca = Marca(id=row['marca_id']).get_by_id()
+            proveedor = Proveedor(id=row['proveedor_id']).get_by_id()
+            marca = Marca.deserializar(marca) if marca else None
+            proveedor = Proveedor.deserializar(proveedor) if proveedor else None
+
+            
+            cursor.execute("SELECT categoria_id FROM articulos_categorias WHERE articulo_id = %s", (self.id,))
+            categoria_ids = [cat['categoria_id'] for cat in cursor.fetchall()]
+            categorias = []
+            for cat_id in categoria_ids:
+                cursor.execute("SELECT * FROM categorias WHERE id = %s", (cat_id,))
+                cat_row = cursor.fetchone()
+                if cat_row:
+                    categorias.append(Categorias.deserializar(cat_row))
+
+            articulo = ArticulosModel(
+                id=row['id'],
+                descripcion=row['descripcion'],
+                precio=row['precio'],
+                stock=row['stock'],
+                marca=marca,
+                proveedor=proveedor,
+                categorias=categorias
+            )
+                       
+            return articulo.serializar()
+     except Exception as exc:
+           print("Error en get_by_id:", exc)
+           return {'mensaje': f" error buscar un articulo {exc}"}
+     finally:
+        cnx.close() 
     def create(self):
         cnx = ConectDB.connect()
         with cnx.cursor(pymysql.cursors.DictCursor) as cursor:
